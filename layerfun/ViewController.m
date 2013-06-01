23f42ec14e5615f8601027eb1e395d1a5e7d6af7
Fred Sharples <fred@orangedesign.com>
2013-05-31 17:50:07 -0700

    added a fade animation

--- layerfun/ViewController.m
+++ layerfun/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 #import <QuartzCore/QuartzCore.h>
 
+static inline double radians (double degrees)
+{
+    return degrees * M_PI/180;
+}
+
+
 @interface ViewController ()
 
 @end
@@ -57,9 +63,92 @@
     imageLayer.masksToBounds = YES;
     [sublayer addSublayer:imageLayer];
     
+    CALayer *customDrawn = [CALayer layer];
+    customDrawn.delegate = self;
+    customDrawn.backgroundColor = [UIColor greenColor].CGColor;
+    customDrawn.frame = CGRectMake(30, 250, 128, 40);
+    customDrawn.shadowOffset = CGSizeMake(0, 30);
+    customDrawn.shadowRadius = 5.0;
+    customDrawn.shadowColor = [UIColor blackColor].CGColor;
+    customDrawn.shadowOpacity = 0.8;
+    customDrawn.cornerRadius = 10.0;
+    customDrawn.borderColor = [UIColor blackColor].CGColor;
+    customDrawn.borderWidth = 2.0;
+    customDrawn.masksToBounds = YES;
+    [self.view.layer addSublayer:customDrawn];
+    [customDrawn setNeedsDisplay];
     
+    [self FadeMyLayer:imageLayer];
+    
+   /* CABasicAnimation* fadeAnim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    fadeAnim.fromValue = [NSNumber numberWithFloat:1.0];
+    fadeAnim.toValue = [NSNumber numberWithFloat:0.0];
+    fadeAnim.duration = 4.0;
+    [imageLayer addAnimation:fadeAnim forKey:@"opacity"];
+    
+    imageLayer.opacity = 0.0;
+    */
     
 }
+
+- (void) FadeMyLayer:(CALayer *)myLayer {
+    CABasicAnimation* fadeAnim = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    fadeAnim.fromValue = [NSNumber numberWithFloat:1.0];
+    fadeAnim.toValue = [NSNumber numberWithFloat:0.0];
+    fadeAnim.duration = 4.0;
+    [myLayer addAnimation:fadeAnim forKey:@"opacity"];
+    
+    myLayer.opacity = 0.0;
+
+    
+}
+void MyDrawColoredPattern (void *info, CGContextRef context) {
+    
+    CGColorRef dotColor = [UIColor colorWithHue:0.0 saturation:0.0 brightness:0.07 alpha:1.0].CGColor;
+    CGColorRef shadowColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.1].CGColor;
+    
+    CGContextSetFillColorWithColor(context, dotColor);
+    CGContextSetShadowWithColor(context, CGSizeMake(0,1),1, shadowColor);
+    
+    CGContextAddArc(context, 3, 3, 4, 0, radians (360.0), 0);
+    CGContextFillPath(context);
+    
+    CGContextAddArc(context, 16, 16, 4, 0, radians (360.0), 0);
+    CGContextFillPath(context);
+    
+}
+
+- (void) drawLayer:(CALayer *)layer inContext:(CGContextRef)context {
+    
+    CGColorRef bgColor = [UIColor colorWithHue:0.6 saturation:1.0 brightness:1.0 alpha:1.0].CGColor;
+    
+    
+    CGContextSetFillColorWithColor(context, bgColor);
+    CGContextFillRect(context, layer.bounds);
+    
+    
+    static const CGPatternCallbacks callbacks = { 0, &MyDrawColoredPattern, NULL };
+    
+    
+    CGContextSaveGState(context);
+    
+    CGColorSpaceRef patternSpace = CGColorSpaceCreatePattern(NULL);
+    CGContextSetFillColorSpace(context, patternSpace);
+    
+    CGColorSpaceRelease(patternSpace);
+    
+    CGPatternRef pattern = CGPatternCreate(NULL, layer.bounds, CGAffineTransformIdentity, 24.0, 24.0, kCGPatternTilingConstantSpacing, true, &callbacks);
+    
+    
+    
+    CGFloat alpha = 1.0;
+    CGContextSetFillPattern(context, pattern, &alpha);
+    CGPatternRelease(pattern);
+    CGContextFillRect(context, layer.bounds);
+    CGContextRestoreGState(context);
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
